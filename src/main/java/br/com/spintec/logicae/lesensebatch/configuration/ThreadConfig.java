ba7952d00c45b4a6491589e2f275085b7e124a19
Lucas <lucas.reis@redequalis.com.br>
2020-02-06 20:38:20 -0300

    1.0.0

--- src/main/java/br/com/spintec/logicae/lesensebatch/configuration/ThreadConfig.java
+++ src/main/java/br/com/spintec/logicae/lesensebatch/configuration/ThreadConfig.java
@@ -15,7 +15,7 @@ public class ThreadConfig {
     public Executor taskExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(6);
-        executor.setMaxPoolSize(6);
+        executor.setMaxPoolSize(200);
         executor.setQueueCapacity(10000);
         executor.setThreadNamePrefix("lesense-batch-");
         executor.initialize();
